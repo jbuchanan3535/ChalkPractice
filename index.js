cc019aae69c330f1e96be53cc959221f445a356c
Jbuchanan35 <63601398+Jbuchanan35@users.noreply.github.com>
2020-11-13 15:25:13 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+//using chalk
+const chalk = require('chalk');
+const log = console.log
+//console.log(chalk.blue('Hello world!'));
+
+//log(chalk.blue(''));
+
+//using chalk-animation
+const chalkAnimation = require('chalk-animation');
+ 
+///chalkAnimation.rainbow('Lorem ipsum dolor sit amet'));
+log(chalk.green('Awoken in the town of Lumbridge you arrive in the animated world with 100 coins and two rations to eat.'));
+log(chalk.blue('You wonder how you ended up here but hear golbins rummaging around in the back, so you rush off.'));
+log(chalk.green('You find someone down the path and they seem to have the same distraught face as you.'));
+log(chalk.blue('After striking up a conversation with him you realize this is not a safe place and you\'d be better off going north.'));
+log(chalk.green('The man ended up feeling bad and gave you 20 coins and a rusty dagger. He wishes you an "intact journey".'));
+log(chalk.red('Going north you fight off some goblins and loot their gold they have stolen from other people.'));
+log(chalk.yellow('Reaching a town up north you find a sword shop and purchase a new blade as well as some healing items.'));
+log(chalk.blue('While leaving the old man outside of the store says "If you want out of this paradox, the key lies within the one that flies high".'));
+log(chalk.yellow('As you were about to ask what he meant he vanishes. Leaving you still going north wondering just who he was reffering to.'));
+log(chalk.red('While eating a trout you purchased back at the shop, everything suddenly starts to begins to be engulfed with flames, your backpack/supplies as well.'));
+log(chalk.red('Running towards the nearby pond for some type of safe place, you look up to see a mythical creature surronding you in an ariel formation.'));
+log(chalk.green('Then you hear a loud ringing sound, eveything begins to slow down. From the heat of the beasts breath to the seconds of time.'));
+log(chalk.green('You begin to realize that ringing sounds identical to your phone alarm...'));
+log(chalk.green('Sitting up in your bed you look into your mirror confused and say "time to go to mrs.markleys class"'));
+chalkAnimation.radar('Happy Monday :)');    // so anything with animation will appear at the bottom bc eveything else is a console log. no animated story sentences
